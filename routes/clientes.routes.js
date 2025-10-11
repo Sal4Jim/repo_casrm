509c7aae652d3bdf754a7dc9ad203d419a9617b6
Diego <diegolezama008@gmail.com>
2025-10-10 22:14:49 -0500

    Revert "Merge branch 'diego' into develop"
    This reverts commit 88896be0ae81bf437495baa1f9a5e01cbb99a49e, reversing
    changes made to 93465c1b39278bc3e0b2c46b94892c648b911f58.

--- routes/clientes.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const clienteController = require('../controllers/cliente.controller');
-
-// Ruta para crear un cliente
-router.post('/', clienteController.createCliente);
-
-module.exports = router;
